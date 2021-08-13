3fa6283894e3feb2a1ad62de0597792fa5c36f3e
mako-shark <83571180+mako-shark@users.noreply.github.com>
2021-08-13 11:44:10 +0800

    tweak: toggle input style

--- packages/pancake-uikit/src/components/PancakeToggle/StyledPancakeToggle.tsx
+++ packages/pancake-uikit/src/components/PancakeToggle/StyledPancakeToggle.tsx
@@ -74,8 +74,7 @@ export const PancakeStack = styled.div<HandleProps>`
 
   .pancake:nth-child(1) {
     background: ${({ theme }) => theme.colors.primary};
-    box-shadow: 0 ${getScale("pancakeThickness")} 0 ${getScale("pancakeThickness")}
-      ${({ theme }) => theme.pancakeToggle.handleShadow};
+    box-shadow: none;
   }
 `;
 
